e46278b2beae05bfd8636e6a95d7c72f251b1bdb
Thùy Trang <thuytrang331@gmail.com>
2022-11-24 16:56:44 +0700

    [221124] - draw canvas

--- web-app/frontend/src/App.tsx
+++ web-app/frontend/src/App.tsx
@@ -1,12 +1,10 @@
 import React from "react"
-
 import { DrawCanvas } from "@comp/canvas/DrawCanvas"
 
 export default function App() {
 
   return (
     <div>
-      <div>Web App</div>
       <DrawCanvas/>
     </div>
   )
--- web-app/frontend/src/components/canvas/DrawCanvas.tsx
+++ web-app/frontend/src/components/canvas/DrawCanvas.tsx
@@ -1,17 +1,93 @@
-import React, { useEffect, useRef} from "react";
+import React, { useEffect, useRef, useState} from "react";
+import { css } from '@emotion/css'
 import { Coordinates } from "@type/index";
-
-
 
 export const DrawCanvas = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null)
+  const [context, setContext] = useState<CanvasRenderingContext2D>()
+
+  useEffect(() => {
+    let mouseDown: boolean = false
+    let start: Coordinates = { x: 0, y: 0}
+    let end: Coordinates = { x: 0, y: 0}
+
+    const handleMouseDown = (e: MouseEvent) => {
+      mouseDown = true
+      const canvasOffsetLeft = canvasRef.current ? canvasRef.current.offsetLeft : 0
+      const canvasOffstTop = canvasRef.current ? canvasRef.current.offsetTop : 0
+
+      start = {
+        x: e.clientX - canvasOffsetLeft,
+        y: e.clientY - canvasOffstTop
+      }
+
+      end = {
+        x: e.clientX - canvasOffsetLeft,
+        y: e.clientY - canvasOffstTop
+      }
+    }
+  
+    const handleMouseUp = (e: MouseEvent) => {
+      mouseDown = false
+    }
+  
+    const handleMouseMove = (e: MouseEvent) => {
+      if (mouseDown && context) {
+        const canvasOffsetLeft = canvasRef.current ? canvasRef.current.offsetLeft : 0
+        const canvasOffstTop = canvasRef.current ? canvasRef.current.offsetTop : 0
+
+        start = {
+          x: end.x,
+          y: end.y
+        }
+
+        end = {
+          x: e.clientX - canvasOffsetLeft,
+          y: e.clientY - canvasOffstTop
+        }
+
+        context.beginPath()
+        context.moveTo(start.x, start.y)
+        context.lineTo(end.x, end.y)
+        context.strokeStyle = 'white'
+        context.lineWidth = 20
+        context.lineCap = 'round'
+        context.stroke()
+        context.closePath()
+      }
+    }
+
+    if (canvasRef.current) {
+      const renderCtx = canvasRef.current.getContext('2d')
+
+      if (renderCtx) {
+        canvasRef.current.addEventListener('mousedown', handleMouseDown)
+        canvasRef.current.addEventListener('mouseup', handleMouseUp)
+        canvasRef.current.addEventListener('mousemove', handleMouseMove)
+        
+        setContext(renderCtx)
+      }
+    }
+
+    return () => {
+      if (canvasRef.current) {
+        canvasRef.current.removeEventListener('mousedown', handleMouseDown)
+        canvasRef.current.removeEventListener('mouseup', handleMouseUp)
+        canvasRef.current.removeEventListener('mousemove', handleMouseMove)
+      }
+    }
+  }, [context])
 
   return (
-    
     <canvas 
       ref={canvasRef}
+      className={sCanvas}
       width={300}
       height={300}
     />
   )
 }
+
+const sCanvas = css`
+  background-color: lightgreen;
+`
